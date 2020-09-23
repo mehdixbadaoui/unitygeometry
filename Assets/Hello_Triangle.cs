61e4c037d348031bc4f6b0464200d14cabef82b4
Mehdi <mehdixbadaoui@outlook.fr>
2020-09-23 22:01:38 +0200

    circle and cylindre work perfectly

--- Assets/Hello_Triangle.cs
+++ Assets/Hello_Triangle.cs
@@ -24,8 +24,9 @@ public class Hello_Triangle : MonoBehaviour
 
         //plan();
 
-        circle(new Vector3(0, 0, 0), 3);
+        // circle(new Vector3(0, 0, 0), 3, 4);
 
+        cylindre(new Vector3(0, 0, 0), 3, 60, 10);
         Mesh msh = new Mesh();
 
         msh.vertices = vertices;
@@ -77,25 +78,23 @@ public class Hello_Triangle : MonoBehaviour
 
     }
 
-    void circle(Vector3 centre, int rayon){
-        int edges = 60;
-        int r = rayon;
-        Vector3 c = centre;
-        int rotate = 10;
+    void circle(Vector3 c, int r, int edges){
+        int rotate = 360 / edges;
         vertices = new Vector3[edges + 1];
-        triangles = new int[111];
+        triangles = new int[ 3*edges];
 
-        int v = 0;
+        int start = 0;
+
         int angle = 0;
-        vertices[0] = new Vector3(0,0,0);
-        for (int i = 1; i < edges; i++)
+        vertices[0] = c;
+        for (int i = 1; i <= edges; i++)
         {
             vertices[i] = new Vector3(r * (float)Math.Cos(angle * Math.PI / 180), 0, r * (float)Math.Sin(angle * Math.PI / 180));
-            angle += 10;
+            angle += rotate;
         }
 
-        int tri = 0;
-        for (int i = 0; i <= (360 / rotate); i++)
+        int tri = start;
+        for (int i = 1; i < edges; i++)
         {
             triangles[tri] = 0;
             triangles[tri+1] = i + 1;
@@ -103,27 +102,88 @@ public class Hello_Triangle : MonoBehaviour
 
         tri += 3;
         }
-        //Debug.Log(tri);
 
-        void cylindre(){
-            int edges = 60;
-            int r;
-            Vector3 ct, cb;
-            int rotate;
-            vertices = new Vector3[2*(edges + 1)];
-            triangles = new int[444];
+        triangles[tri] = start;
+        triangles[tri+1] = start + 1;
+        triangles[tri+2] = edges;
 
-        for (int i = 1; i < edges; i++)
+
+    }
+
+    void cylindre(Vector3 c, float r, int edges, float height){
+
+        // int     void circle(Vector3 c, int r, int edges){
+        int rotate = 360 / edges;
+        vertices = new Vector3[2* (edges + 1)];
+        triangles = new int[ 12*edges];
+
+
+        int angle = 0;
+        vertices[0] = c;
+        vertices[edges + 1] = c + new Vector3(0, height, 0);
+
+        for (int i = 1; i <= edges; i++)
         {
             vertices[i] = new Vector3(r * (float)Math.Cos(angle * Math.PI / 180), 0, r * (float)Math.Sin(angle * Math.PI / 180));
-            angle += 10;
-        }
-
-
+            vertices[edges + 1 + i] = new Vector3(r * (float)Math.Cos(angle * Math.PI / 180), height, r * (float)Math.Sin(angle * Math.PI / 180));
+            angle += rotate;
 
         }
-        
+        int start_bottom = 0;
+        int start_top = edges + 1;
 
+        int tri = 0;
+        for (int i = 1; i < edges; i++)
+        {
+            triangles[tri] = start_bottom;
+            triangles[tri+2] = i + 1;
+            triangles[tri+1] = i;
+
+            triangles[tri+3] = start_top;
+            triangles[tri+4] = i + edges +2;
+            triangles[tri+5] = i + edges + 1;
+
+            triangles[tri+6] = i;
+            triangles[tri+8] = i + edges + 2;
+            triangles[tri+7] = i + edges + 1;
+
+            triangles[tri+9] = i;
+            triangles[tri+10] = i + edges +2;
+            triangles[tri+11] = i + 1;
+
+
+
+        tri += 12;
+        }
+
+        triangles[tri] = start_bottom;
+        triangles[tri+1] = edges;
+        triangles[tri+2] = start_bottom + 1;
+
+        triangles[tri+3] = start_top;
+        triangles[tri+4] = start_top + 1;
+        triangles[tri+5] = 2*edges + 1;
+
+        triangles[tri+6] = start_bottom + 1;
+        triangles[tri+7] = edges;
+        triangles[tri+8] = 2*edges + 1;
+
+        triangles[tri+9] = start_bottom + 1;
+        triangles[tri+10] = 2*edges + 1;
+        triangles[tri+11] = start_top + 1;
+
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null) return;
+        // DrawSphere();
+        Gizmos.color = Color.black;
+        foreach (var point in vertices)
+        {
+            Gizmos.DrawSphere(point, 0.1f);
+        }
     }
 
     
